f63544eb1cda430d9d6bdbc81184aedbe31e1fc7
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2022-11-13 17:04:50 -0300

    Video 7.2

--- src/main/java/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/com/alura/mvc/mudi/controller/HomeController.java
@@ -5,22 +5,42 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.alura.mvc.mudi.model.Pedido;
+import com.alura.mvc.mudi.model.StatusPedido;
 import com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 	
 	@Autowired
 	PedidoRepository pedidoRepository;
 	
-	@GetMapping("/home")
+	@GetMapping
 	public String home(Model model) {
 		
 		List<Pedido> pedidos = pedidoRepository.findAll();
 		model.addAttribute("pedidos",pedidos);
 		return "home";
+	}
+	
+
+	@GetMapping("/{status}")
+	public String porStatus(@PathVariable("status") String status,Model model) {
+		
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
+		model.addAttribute("pedidos",pedidos);
+		model.addAttribute("status", status);
+		return "home";
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String onError() {
+		return "redirect:/home";
 	}
 }
--- src/main/java/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -1,11 +1,16 @@
 package com.alura.mvc.mudi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.alura.mvc.mudi.model.Pedido;
+import com.alura.mvc.mudi.model.StatusPedido;
 
 @Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Long>{	
+public interface PedidoRepository extends JpaRepository<Pedido, Long>{
+
+	List<Pedido> findByStatus(StatusPedido status);	
 	
 }
